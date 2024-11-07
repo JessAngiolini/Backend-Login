274b6420b482bedbe5301a8505d6921624ddd2c0
JessAngiolini <jlangiolini@gmail.com>
2024-11-07 19:19:27 -0300

    changes routes

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,8 +1,10 @@
 import {Router} from "express";
-import { saveRegister, LoginCtrl, } from '../controllers/usersControllers';
+import { saveRegister, LoginCtrl, recoverPassword, resetPassword } from '../controllers/usersControllers';
 
 const router = Router();
 router.post("/register", saveRegister)
 router.post('/login', LoginCtrl) 
+router.post('/recover-password', recoverPassword);
+router.put('/reset-password',resetPassword);
 
 export default router; 
